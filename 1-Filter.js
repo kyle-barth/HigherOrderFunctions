dca37252b8e9ddd03b11e131ca17a31b9bc49e38
Kyle Barth <40353515+kyle-barth@users.noreply.github.com>
2018-07-05 14:40:46 +0100

    Add files via upload

---
+++ 1-Filter.js
@@ -0,0 +1,24 @@
+var animals = [
+  { name: 'bella' , species: 'cat' },
+  { name: 'donut' , species: 'cat' },
+  { name: 'kiwi' , species: 'dog' },
+  { name: 'luna' , species: 'bird' }
+]
+
+// Boring way of doing things
+// var cats = []
+// for (var i = 0; i < animals.length; i++) {
+//   if (animals.species === 'cat')
+//     cats.push(animals[i])
+// }
+
+// Callback function example -> function(animal) is the callback function
+var dogs = animals.filter(function(animal) {
+  return animal.species === 'cat'
+})
+
+// Same as doing this ->
+// var isCat = function(animal) {
+//   return animal.species === 'cat'
+// }
+// var dogs = animals.filter(isCat)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# HigherOrderFunctions
-Practice with Higher Order Functions in JavaScript
+# Higher Order Functions
+
+Examples of how to use higher order functions in JavaScript following an online series: https://www.youtube.com/watch?v=BMUiFMZr7vk
+
+Higher Order Functions
+- Filter
+- Map
+- Reduce
+
+JavaScript Theory
+- Callback functions
+- ES6 Syntax
